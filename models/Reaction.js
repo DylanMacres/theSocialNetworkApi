da0df67ce0e5bd8c05d95523c7b0f02419616afc
DylanMacres <Dylan.Macres@du.edu>
2022-11-13 23:44:34 -0700

    seperating my thought and reaction schemas

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { max } = require('moment/moment');
+const { Schema, Types, model } = require('mongoose');
+
+//reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        username: {
+            type: String,
+            required:true,
+        } ,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+        
+    }
+)
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -2,33 +2,18 @@ const { max } = require('moment/moment');
 const { Schema, Types, model } = require('mongoose');
 
 
-
-const reactionSchema = new Schema(
+//thought schema
+const thoughtSchema = new Schema(
     {
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        username: {
-            type: String,
-            required:true,
-        } ,
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-    },
+
+    }
+  
     {
         toJSON: {
             virtuals: true,
             getters: true
         },
         id: false,
-        
+
     }
 )
