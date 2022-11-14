fc9714380300f704b8cc493144245fd637829a39
DylanMacres <Dylan.Macres@du.edu>
2022-11-13 23:03:27 -0700

    setting the user models

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+
+//schema for the user model
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email:{
+            type: String,
+            unique: true,
+            required: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+                "Please input a valid email address!"
+            ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// making the user model using the schema
+const User = model('User', userSchema);
+
+//export the user 
+module.exports = User;
