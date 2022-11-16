f4d8f437fe9c5359af880d16bb0b692c80d99bef
DylanMacres <Dylan.Macres@du.edu>
2022-11-15 20:27:04 -0700

    adding the index to routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -13,23 +13,27 @@ const {
 
 } = require('../../controllers/thoughtController');
 
+
+ // /api/thoughts
 router 
     .route('/')
     .get(getAllThoughts)
     .post(createThought)
   
-
+    // /api/thoughts/:thoughtId
 router 
     .route('/:thoughtId')
     .get(getSingleThought)
     .put(updateThought)
     .delete(deleteThought)
 
-
+    // /api/thoughts/:thoughtId/reactions
 router 
     .route('/:thoughtId/reactions')
     .post(createReaction)
 
+
+    // /api/thoughts/:thoughtId/:reactionId
 router
     .route('/:thoughtId/:reactionId')
     .delete(deleteReaction)
