877b1436e49fd5db20b7ef026b04a28de4625cfc
DylanMacres <Dylan.Macres@du.edu>
2022-11-15 17:23:05 -0700

    surprise surpise adding to the user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,32 @@ module.exports = {
         User.find()
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err))
-    }
+    },
+
+    //creating the user
+    createUser(req,res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err))
+    },
+
+    //updating user
+    updateUser(req, res) {
+        User.findByIdAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+        !user 
+        ? res.status(404).json({ message: "Couldn't find that user, sorry!:/"})
+        : res.json(user)
+
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+
 
 
 
