5b309b1a527028aa7ed115d12a56c007a7f43b51
DylanMacres <Dylan.Macres@du.edu>
2022-11-15 20:00:44 -0700

    adding the routes to the api

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+
+const { 
+    getOneUser,
+    getAllUsers, 
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+
+
+
+} = require('../../controllers/userController')
+
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+
+// /api/users/:userId
+router
+    .route('/:userId')
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+    // /api/users/userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+module.exports = router;
